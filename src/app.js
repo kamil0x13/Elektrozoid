4a8af8a5abc62cc43475b2d0df8366de4ff5456c
Kamil Pietrzak <pietrzakkamil013@gmail.com>
2022-04-08 23:31:00 +0200

    adding route

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json())
 //Routers
 require('./routers/user')(app)
 require('./routers/admin')(app)
+require('./routers/product')(app)
 
 app.use(express.static(path.join(__dirname, 'build')))
 
